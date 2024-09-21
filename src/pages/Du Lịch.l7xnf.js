91a64594d4eccd440754d6067b569858ae0be4d0
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmail.com>
2024-09-21 16:32:02 +0700

    update repeater

--- src/pages/Du Lịch.l7xnf.js
+++ src/pages/Du Lịch.l7xnf.js
@@ -1,10 +1,10 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+$w("#repeaterintop").onItemReady(($item, itemData, index) => {
+    let originalText = itemData.price;
+    let modifiedText = originalText === 0 ? 'Miễn phí' : originalText;
+    console.log(modifiedText);
+    $item("#price").text = modifiedText;
+  });
 
-$w.onReady(function () {
-    // Write your JavaScript here
 
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
-});
+  
+  
